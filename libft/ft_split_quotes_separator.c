acdb3d99f6728a818e585a26ac7337d653e70c16
JRM32 <Javier.rollon@gmail.com>
2025-05-02 18:49:11 +0200

    array spaced casi bien pero falta fix clean_nulls

--- libft/ft_split_quotes_separator.c
+++ libft/ft_split_quotes_separator.c
@@ -6,18 +6,17 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:21:17 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/05/02 16:37:14 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/05/02 18:46:14 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "../inc/minishell_j.h" //javi
 
-void	init_separator(t_split *squotes, size_t *i, size_t *j)//, size_t *k)
+void	init_separator(t_split *squotes, size_t *i, size_t *j)
 {
 	*i = 0;
 	*j = 0;
-	//*k = 0;
 	if (squotes->s[squotes->start] == '"' && !(squotes->quotes % 2))
 	{
 		squotes->c = '"';
@@ -39,19 +38,19 @@ void	init_separator(t_split *squotes, size_t *i, size_t *j)//, size_t *k)
 
 void	run_spaces_or_one_quote(t_split *squotes, t_input *input)
 {
+	input->is_spaced = 0;
+	if (squotes->start != 0 && squotes->s[(squotes->start) - 1] == ' ')
+		input->is_spaced = 1;
 	if (squotes->c != '"' && squotes->c != '\'')
 	{
 		while ((squotes->s[squotes->start] == squotes->c)
 			&& (squotes->s[squotes->start]))
 			(squotes->start)++;
+		if (squotes->start != 0 && squotes->s[(squotes->start) - 1] == ' ')
+			input->is_spaced = 1;
 	}
 	else if (squotes->c == '"' || squotes->c == '\'')
-	{
-		if (squotes->quotes % 2 && squotes->start != 0
-			&& squotes->s[(squotes->start) - 1] == ' ')
-			input->is_spaced = 1;
 		(squotes->start)++;
-	}
 }
 
 void	open_close_quotes(t_split *squotes)
@@ -105,9 +104,9 @@ char	*sub_split_quotes(t_split *sq, t_input *input)
 	init_separator(sq, &i, &j);
 	run_spaces_or_one_quote(sq, input);
 	open_close_quotes(sq);
-	if (sq->quotes > 0 && sq->quotes % 2 == 0
+	/* if (sq->quotes > 0 && sq->quotes % 2 == 0
 		&& sq->c == ' ' && sq->s[(sq->start) - 1] == ' ')
-		input->is_spaced = 1;
+		input->is_spaced = 1; */
 	while (sq->s[i + sq->start] && sq->s[i + sq->start] != sq->c)
 	{
 		if (sq->c == ' ' && (sq->s[i + sq->start] == '"'
