779599eb97b54d69ddd84c240133cbcd8df9a4b0
J4P032 <J4p032@gmail.com>
2025-05-08 23:42:51 +0200

    pesadilla de echo expandido

--- src/bi_echo_m.c
+++ src/bi_echo_m.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 18:50:16 by mpico-bu          #+#    #+#             */
-/*   Updated: 2025/05/08 12:30:04 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/05/08 23:39:52 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,11 +69,25 @@ void	manage_dollar(t_input *in, size_t w, int spaced)
 	size_t	j;
 	size_t	dollars;
 	int		env_n;
+	size_t	idollar;//
 
 	i = 0;
 	dollars = 0;
-	if (spaced)
-		printf(" ");
+	
+	while (in->input_split[w][i] != '$')
+		i++;
+	idollar = i;
+	while (in->input_split[w][i] == '$')
+		i++;
+	env_n = valid_env((in->input_split[w] + i), in, w);	
+		
+	if (env_n < 0 && (ft_isalpha(in->input_split[w][i]) || in->input_split[w][i] == '_') && !idollar && is_quoted(in, w) != 2)
+		;
+	else if (spaced)
+	printf(" ");
+	
+	
+	i = 0;
 	while (in->input_split[w][i])
 	{
 		if (in->input_split[w][i] != '$')
@@ -85,6 +99,7 @@ void	manage_dollar(t_input *in, size_t w, int spaced)
 				i++;
 				dollars++;
 			}
+			idollar = i + 1;//
 			env_n = valid_env((in->input_split[w] + i + 1), in, w);
 			if (env_n > -1)
 			{
@@ -107,12 +122,22 @@ void	manage_dollar(t_input *in, size_t w, int spaced)
 			if (in->input_split[w][i])
 				i++;
 			while (in->input_split[w][i] && in->input_split[w][i + 1] != ' ' && in->input_split[w][i] != '$')
-			{
-				char c = in->input_split[w][i];
-				(void)c;
 				i++;
-			}
-			if ((!(dollars % 2) && env_n < 0))
+			if (env_n < 0 && (ft_isalpha(in->input_split[w][idollar]) || in->input_split[w][idollar] == '_'))
+				;
+			//{
+				//if (w < in->input_words && !is_quoted(in, w) && in->status[w] == EPTY_SP)
+				//{
+					//in->spaced = 0;/////////////////////////////////////////////////
+					/* 	if (in->status[w + 1] == EPTY_SP)
+						in->status[w + 1] = EPTY_NSP;
+					else if (in->status[w + 1] == SQUO_SP)
+						in->status[w + 1] = SQUO_NSP;
+					else if (in->status[w + 1] == DQUO_SP)
+						in->status[w + 1] = DQUO_NSP; */
+				//}
+			//}
+			else if (env_n < 0 && !(dollars % 2))
 				printf("$");
 		}
 		if (in->input_split[w][i] && in->input_split[w][i] != '$')
@@ -124,6 +149,7 @@ void	print_arguments(t_input *in, size_t	w, int spaced)
 {
 	int		print_as_env;
 
+	//in->spaced = 1;////////////////////////////////////////////////////////////
 	print_as_env = (is_quoted(in, w) == 2 || !is_quoted(in, w));
 	if (spaced)
 	{
@@ -156,7 +182,7 @@ void	ft_echo(t_input *in)
 	{
 		if ((in->status[i] == EPTY_SP || in->status[i] == SQUO_SP
 				|| in->status[i] == DQUO_SP) && in->input_split[i][0]
-				&& i != start)
+				&& i != start)// && in->spaced)///////////////////////////////////////////////
 			print_arguments(in, i, 1);
 		else if ((in->status[i] == EPTY_SP || in->status[i] == SQUO_SP
 				|| in->status[i] == DQUO_SP) && in->input_split[i][0] == '\0'
