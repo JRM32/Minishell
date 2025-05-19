3ef3b61f1fc0e8612b07c2a324ce8ee86b66f90f
JRM32 <Javier.rollon@gmail.com>
2025-05-19 11:48:20 +0200

    ya hace export kk=$USER

--- src/bi_export_m.c
+++ src/bi_export_m.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bi_export_m.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mpico-bu <mpico-bu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 19:06:31 by mpico-bu          #+#    #+#             */
-/*   Updated: 2025/05/11 20:51:22 by mpico-bu         ###   ########.fr       */
+/*   Updated: 2025/05/19 11:43:12 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,12 @@ bool	ft_manage_shlvl(char *input, char **envp, int i, int len)
 	return (1);
 }
 
+/*Check if the variable already is in the **envp.*/
+/*1. if the input dont have an '=' it will exit with error 1 so nothing to do*/
+/*2. search if the name of the variable in input is in the envp if not will...*/
+/*...return 0 so will continue with code. If find it:*/
+/*A. if find SHLVL will process it in a different funcion.*/
+/*B. other will free it first to replace it with the new value (input) of it*/
 bool	ft_check_variables(char *input, char **envp)
 {
 	int		i;
@@ -50,7 +56,7 @@ bool	ft_check_variables(char *input, char **envp)
 			free (envp[i]);
 			envp[i] = ft_strdup(input);
 			if (!envp[i])
-				return (0);
+				return (0); //CUIDADO AQUI!!! YO SALDRIA Y LIBERARIA TODO por que si no el programa continua como si nada y devuelve esa posicion como NULL lo cual habria dos NULL en envp y claro BUG
 			return (1);
 		}
 		i++;
@@ -58,6 +64,9 @@ bool	ft_check_variables(char *input, char **envp)
 	return (0);
 }
 
+/*1. checks if the variable already exists in envp if so, exit*/
+/*2. if not create a new **new_env VAR with space for the new VAR plus the...*/
+/*...the final NULL*/
 void	ft_export(char *input, char ***envp)
 {
 	int		i;
@@ -72,15 +81,15 @@ void	ft_export(char *input, char ***envp)
 		env_position++;
 	new_env = ft_calloc(env_position + 2, sizeof(char *));
 	if (!new_env)
-		return ;
-	while (i < env_position)
+		return ; //LIBERARMOS TODO Y FUERA CLEAN_ALL
+	while (i < env_position) //SI SALE DE CHECK_VARIABLE con 0 al fallar el strdup no llegara al final de todo envp ya que uno por medio sera NULL
 	{
 		new_env[i] = ft_strdup((*envp)[i]);
 		if (!new_env[i++])
 			return (ft_matrix_free(new_env));
 	}
 	new_env[i++] = ft_strdup(input);
-	if (new_env[i])
+	if (new_env[i])//NO LO ENTIENDO (JAVI). si fuera if (envp[i]) si por que podriamos detectar el no NULL del fallo de envp pero si por si falla este strdup aqui, si se hacho sobre una i = 2, aqui estas compararon i = 3, ya que se incrementó en el strdup
 		return (ft_matrix_free(new_env));
 	ft_matrix_free(*envp);
 	*envp = new_env;
--- src/manage_inputs_m.c
+++ src/manage_inputs_m.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 19:28:00 by mpico-bu          #+#    #+#             */
-/*   Updated: 2025/05/17 17:33:50 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/05/19 11:44:25 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void	ft_manage_input(t_input *input, int in_fd, int out_fd)
 	else if (ft_strcmp(input->command, "echo") == 0)
 		echo_short(input);
 	else if (ft_strcmp(input->command, "export") == 0 && input->input_split[1])//mirar cambiar a && input->args[0] (es decir que no sea \0). Pero!! export sin mas lo que hace es poner "declare -x " delante de todas las variables de entorno
-		ft_export(input->args, &input->envp);
+		ft_export(input->parsed, &input->envp);
 	else if (ft_strcmp(input->command, "env") == 0)
 		ft_env(input, input->envp);
 	else if (ft_strcmp(input->command, "unset") == 0 && input->input_split[1])//mirar cambiar a && input->args[0] (es decir que no sea \0). Pero!! unset sin mas lo que hace es poner una nueva linea de prompt
