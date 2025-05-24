15a33683d7d96edce9617fa6aad58cd570e1a3f0
J4P032 <J4p032@gmail.com>
2025-05-24 23:25:27 +0200

    update test.sh con nuevos errores

--- Tester_Japo/tester2.sh
+++ Tester_Japo/tester2.sh
@@ -161,8 +161,14 @@ run_test 'echo $?' 'echo $?'
 run_test "echo \"'\$?'\"" "echo \"'\$?'\""
 run_test "echo '\"hola\"'" "echo '\"hola\"'"
 run_test "echo \" \" \$USER \" \"" "echo \" \" \$USER \" \""
-
 run_test 'echo $a " " a' 'echo $a " " a'
 run_test 'echo "$ a "' 'echo "$ a "'
+run_test 'echo patata "" "" "" tomate' 'echo patata "" "" "" tomate'
+run_test 'echo $a patata $a "" "" "" "" tomate' 'echo $a patata "" "" "" "" tomate'
+run_test 'echo patata $a "" "" "" "" tomate' 'echo patata $a "" "" "" "" tomate'
+run_test 'echo patata $a "" "" "" "" $a tomate' 'echo patata $a "" "" "" "" $a tomate'
+run_test 'echo patata $a """""""" $a tomate' 'echo patata $a """""""" $a tomate'
 
-
+run_test 'echo ""$a""t' 'echo ""$a""t'
+run_test 'echo ""$a"" t' 'echo ""$a"" t'
+run_test 'echo ""$a" " t' 'echo ""$a" " t'
