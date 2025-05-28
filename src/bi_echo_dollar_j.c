a4cc96419f65aae8176ffa33d3cd89e68d0bee20
JRM32 <Javier.rollon@gmail.com>
2025-05-28 13:30:32 +0200

    arreglando normi bi_echo_dollar_j

--- src/bi_echo_dollar_j.c
+++ src/bi_echo_dollar_j.c
@@ -6,7 +6,7 @@
 /*   By: jrollon- <jrollon-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 00:12:44 by jrollon-          #+#    #+#             */
-/*   Updated: 2025/05/28 12:32:07 by jrollon-         ###   ########.fr       */
+/*   Updated: 2025/05/28 13:29:28 by jrollon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -143,29 +143,14 @@ void	print_valid_env_variable(t_input *n, size_t w, size_t *i)
 		(*i)++;
 	}
 }
-
-/*echo $$USER will print USER. $$USERp will be USERp. That is the first while*/
-/*$ will be printed only if $ are ODD (1,3,5...). Here first $ is not...*/
-/*...counted so to be ODD it has to be a EVEN number. That is the reason...*/
-/*...of !(in->dollars % 2). BUT also can only be printed if after the $...*/
-/*...(so idollar index) is not a possible valid ENV (starting with alpha...*/
-/*...or a '_'. That is the reason I do nothing with it.*/
-/*env_n = -2 is when there is an $? so print the exit code. It behaves as...*/
-/*...BASH where echo $???msg will be number???msg*/
-void	print_invalid_envs(t_input *in, size_t w, size_t *i, int env_n)
+/*echo $$USER will print USER. $$USERp will be USERp.*/
+/*j < in->env_len) is because $$2msg $$%msg to stop while print when not valid*/
+/*break necesary to echo "$USER's not print the last R of USER. The i--..."*/
+/*...to print the ' of same case*/
+void	run_chars_after_the_dollar(t_input *in, size_t w, size_t *i, size_t *j)
 {
-	//size_t	env_len;
-	size_t	j;
-
-	j = 0;
-	if (env_n > -1)
-		in->env_len = validlen_env(in->envp[env_n], '=');
-	else if (env_n == -1)
-		in->env_len = invalidlen_env(in->input_split[w] + (*i));
-	while (in->input_split[w][*i]
-		&& in->input_split[w][(*i) + 1] != ' '
-		&& in->input_split[w][*i] != '$'
-		&& env_n != -2)//cuidado ese -2 nuevo que es $?
+	while (in->input_split[w][*i] && in->input_split[w][(*i) + 1] != ' '
+		&& in->input_split[w][*i] != '$' && in->env_n != -2)
 	{
 		if (is_quoted(in, w) == 2 && in->input_split[w][*i] == '\'')
 		{
@@ -173,9 +158,10 @@ void	print_invalid_envs(t_input *in, size_t w, size_t *i, int env_n)
 				(*i)--;
 			break ;
 		}
-		if (in->dollars > 0 && (in->dollars % 2) && (j < in->env_len))
+		if (in->dollars > 0 && (in->dollars % 2) && (*j < in->env_len))
 			ft_printf("%c", in->input_split[w][*i]);
-		else if (j >= in->env_len && !ft_isdigit(in->input_split[w][in->idollar])
+		else if (*j >= in->env_len 
+			&& !ft_isdigit(in->input_split[w][in->idollar])
 			&& !ft_strrchr(D_Y_ODDCHAR, in->input_split[w][in->idollar])
 			&& !ft_strrchr(N_ODDCHAR, in->input_split[w][in->idollar]))
 		{
@@ -185,16 +171,34 @@ void	print_invalid_envs(t_input *in, size_t w, size_t *i, int env_n)
 		}	
 		if (in->input_split[w][*i] && in->input_split[w][*i] != '?')
 			(*i)++;
-		j++;
+		(*j)++;
 	}
-	
-	if (env_n < 0
+}
+
+/*$ will be printed only if $ are ODD (1,3,5...). Here first $ is not...*/
+/*...counted so to be ODD it has to be a EVEN number. That is the reason...*/
+/*...of !(in->dollars % 2). BUT also can only be printed if after the $...*/
+/*...(so idollar index) is not a possible valid ENV (starting with alpha...*/
+/*...or a '_'. That is the reason I do nothing with it.*/
+/*env_n = -2 is when there is an $? so print the exit code. It behaves as...*/
+/*...BASH where echo $???msg will be number???msg*/
+void	print_invalid_envs(t_input *in, size_t w, size_t *i)
+{
+	size_t	j;
+
+	j = 0;
+	if (in->env_n > -1)
+		in->env_len = validlen_env(in->envp[in->env_n], '=');
+	else if (in->env_n == -1)
+		in->env_len = invalidlen_env(in->input_split[w] + (*i));
+	run_chars_after_the_dollar(in, w, i, &j);
+	if (in->env_n < 0
 		&& (ft_isalpha(in->input_split[w][in->idollar])
 		|| in->input_split[w][in->idollar] == '_'))
 		print_rest_no_env(in, w, i);
-	else if (env_n == -1)
+	else if (in->env_n == -1)
 		print_rare_cases(in, w, i);
-	else if (env_n == -2)
+	else if (in->env_n == -2)
 	{
 		(*i)++;
 		if (!(in->dollars % 2))
@@ -202,7 +206,6 @@ void	print_invalid_envs(t_input *in, size_t w, size_t *i, int env_n)
 		else 
 			ft_printf("?");
 		while (in->input_split[w][*i] && in->input_split[w][*i] != '$')
-		//&& in->input_split[w][(*i) + 1] != ' ' // por un error en echo "$?p " o "$? p"
 			ft_printf("%c", in->input_split[w][(*i)++]);
 	}
 }
@@ -237,7 +240,7 @@ void	manage_dollar(t_input *in, size_t w, int spaced)
 			}
 			in->idollar = i + 1;
 			print_valid_env_variable(in, w, &i);
-			print_invalid_envs(in, w, &i, in->env_n);
+			print_invalid_envs(in, w, &i);
 		}
 		if (in->input_split[w][i] && in->input_split[w][i] != '$')
 			check_previous_dollar(in, w, &i);
