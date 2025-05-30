3c7b45522bd02966196a48cba684df4d6adc1690
Davter <mario.pico.busquier@gmail.com>
2025-05-30 14:52:01 +0200

    normi cd

--- src/bi_cd_m.c
+++ src/bi_cd_m.c
@@ -6,18 +6,44 @@
 /*   By: mpico-bu <mpico-bu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 12:07:20 by mpico-bu          #+#    #+#             */
-/*   Updated: 2025/05/22 22:16:10 by mpico-bu         ###   ########.fr       */
+/*   Updated: 2025/05/30 14:49:14 by mpico-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell_m.h"
 #include "../inc/minishell_j.h"
 
+static char	*get_cd_path(t_input *input, char *oldpwd)
+{
+	if (!input->parsed[0])
+	{
+		if (!getenv("HOME"))
+		{
+			printf("cd: HOME not set\n");
+			input->last_exit_code = 1;
+			return (NULL);
+		}
+		return (getenv("HOME"));
+	}
+	if (ft_strcmp(input->parsed, "-") == 0)
+	{
+		if (ft_strlen(oldpwd) == 0)
+		{
+			printf("cd: OLDPWD not set\n");
+			input->last_exit_code = 1;
+			return (NULL);
+		}
+		printf("%s\n", oldpwd);
+		return (oldpwd);
+	}
+	return (input->parsed);
+}
+
 void	ft_cd(t_input *input)
 {
-	char		*path;
-	static char	oldpwd[4096] = "";
-	char		prev[4096];
+	char			*path;
+	static char		oldpwd[4096] = "";
+	char			prev[4096];
 
 	if (input->split_exp[1] && input->split_exp[2])
 	{
@@ -25,43 +51,17 @@ void	ft_cd(t_input *input)
 		input->last_exit_code = 1;
 		return ;
 	}
-	if (!input->parsed[0])
-	{
-		path = getenv("HOME");
-		if (!path)
-		{
-			printf("cd: HOME not set\n");
-			input->last_exit_code = 1;
-			return ;
-		}
-	}
-	else if (ft_strcmp(input->parsed, "-") == 0)
-	{
-		if (ft_strlen(oldpwd) == 0)
-		{
-			printf("cd: OLDPWD not set\n");
-			input->last_exit_code = 1;
-			return ;
-		}
-		printf("%s\n", oldpwd);
-		path = oldpwd;
-	}
-	else
-		path = input->parsed;
-
 	if (getcwd(prev, sizeof(prev)) == NULL)
-		{
-			printf("getcwd");
-			input->last_exit_code = 1;
-			return ;
-		}
+		return ((void)(perror("getcwd"), input->last_exit_code = 1));
+	path = get_cd_path(input, oldpwd);
+	if (!path)
+		return ;
 	if (chdir(path) != 0)
-		{
-			perror("cd");
-			input->last_exit_code = 1;
-			return ;
-		}
+	{
+		input->last_exit_code = 1;
+		perror("cd");
+		return ;
+	}
 	ft_strlcpy(oldpwd, prev, sizeof(oldpwd));
 	input->last_exit_code = 0;
 }
-
